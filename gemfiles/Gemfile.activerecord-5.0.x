f46cb1d1e39e059cbc95968b6f4655021ff51783
Francisco Fullenkamp <francisco000@gmail.com>
2017-05-10 02:19:43 -0300

    monkeypatch

--- gemfiles/Gemfile.activerecord-5.0.x
+++ gemfiles/Gemfile.activerecord-5.0.x
@@ -2,9 +2,9 @@ source "https://rubygems.org"
 
 gemspec :path => '..'
 
-gem "activerecord", "~> 5.0.0"
+gem "activerecord", "~> 5.1.0"
 gem "pg", "~> 0.15"
 
 unless ENV['CI'] || RUBY_PLATFORM =~ /java/
-  gem 'byebug'
+gem 'byebug'
 end
--- lib/postgres_ext/active_record/relation.rb
+++ lib/postgres_ext/active_record/relation.rb
@@ -1,16 +1,16 @@
 gdep_4_2 = Gem::Dependency.new('activerecord', '>= 4.2.0')
 ar_4_2_version_cutoff = gdep_4_2.matching_specs.sort_by(&:version).last
 
-gdep_5_0 = Gem::Dependency.new('activerecord', '~> 5.0.0')
+gdep_5_0 = Gem::Dependency.new('activerecord', '~> 5.1.0')
 ar_5_0_version_cutoff = gdep_5_0.matching_specs.sort_by(&:version).last
 
 require 'postgres_ext/active_record/relation/merger'
 require 'postgres_ext/active_record/relation/query_methods'
 
 if ar_5_0_version_cutoff
-  require 'postgres_ext/active_record/5.0/relation/predicate_builder/array_handler'
+	require 'postgres_ext/active_record/5.0/relation/predicate_builder/array_handler'
 elsif ar_4_2_version_cutuff
-  require 'postgres_ext/active_record/relation/predicate_builder/array_handler'
+	require 'postgres_ext/active_record/relation/predicate_builder/array_handler'
 else
-  require 'postgres_ext/active_record/4.x/relation/predicate_builder'
+	require 'postgres_ext/active_record/4.x/relation/predicate_builder'
 end
